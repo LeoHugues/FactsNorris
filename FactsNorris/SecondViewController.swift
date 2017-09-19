6559e671604e89f0d9f6d650b843fbc083667dac
julien_mathe <mathe.julien@hotmail.fr>
2017-09-19 16:32:01 +0200

    first commit

---
+++ FactsNorris/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FactsNorris
+//
+//  Created by Julien Mathé on 19/09/2017.
+//  Copyright © 2017 Julien Mathé. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
